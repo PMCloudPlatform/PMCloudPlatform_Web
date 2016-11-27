b13acd42d22ab01e4e89cf8853816641af0b4bcc
ShuiShuiMeng <981904597@qq.com>
2016-11-27 15:41:09 +0800

    去掉了川普 做好了index页面 data和map的导航栏已添加

--- public/javascripts/bind/data.js
+++ public/javascripts/bind/data.js
@@ -1,22 +1,3 @@
-var testVM = new Vue({
-	el:'#test',
-	data:{
-		message:1,
-		cMessage:1
-	},
-	methods:{
-		plusOne: function(){
-			this.message += 1 
-		}
-	},
-	computed:{
-		computedMessage: function(){
-			this.cMessage += 1;
-			return this.cMessage + this.message;
-		}
-	}
-});
-
 var timeVM = new Vue({
 	el:'#selector',
 	data:{
