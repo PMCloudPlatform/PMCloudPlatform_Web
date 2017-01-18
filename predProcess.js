e01dad3b8ffc0d3298cdc0efe9604fb644b948e9
guyao <gamesgao@163.com>
2017-01-18 10:33:08 +0800

    添加预测程序

--- predProcess.js
+++ predProcess.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var cp = require('child_process');
 var pred = {};
+var dataSetFile = 'testdata.txt';
 var predFile = "predict.exe";
 
 pred.lock = false;
@@ -10,7 +11,23 @@ pred.runPrediction = function(lot, lat, time, callback){
     while(pred.lock == true){
         sleep(1);
     }
+    data = [];
+    pmDate = Date(time);
+    data[0] = pmDate.getHours()/24;
+
+    data[1] = 0;
+    for(i = 0; i < pmDate.getMonth();i++){
+        data[1] += (new Date(pmDate.getFullYear(), i, 0)).getDate();
+    }
+    data[1] += pmDate.getDate();
+    data[1] = data[1]/365;
+    data[2] = lat;
+    data[2] = data[2]/180
+    data[3] = lot;
+    data[3] = data[3]/360
     pred.lock = true;
+    
+    fs.writeFileSync(dataSetFile, data.join(" ")+'\n',"UTF-8");
     child = cp.exec(predFile, function(error, stdout, stderr){
         pred.lock = false;
         if (error) {
