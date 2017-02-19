941ffa521e490474936ddbdaff1415d772e5a4a0
guyao <gamesgao@163.com>
2017-02-19 20:05:10 +0800

    最终版本，不知道改了哪里www

--- PointGeoJSON.js
+++ PointGeoJSON.js
@@ -1,31 +1,38 @@
 {
     "type": "FeatureCollection",
-    "features": [
-    {
-        "type": "Feature",
-        "geometry": {
-        "type": "Point",
-        "coordinates": [121.4369081, 31.0236681]
+    "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [121.4369081, 31.0236681]
+            },
+            "properties": {
+                "size": 76
+            }
         },
-        "properties": {"size": 76}},
-    {
-        "type": "Feature",
-        "geometry": {
-        "type": "Point",
-        "coordinates": [121.4369081, 31.0246681]
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [121.4369081, 31.0246681]
+            },
+            "properties": {
+                "size": 543
+            }
         },
-        "properties": {"size": 543}},    
-    ]   
+    ]
 }
 
 {
-        "type": "FeatureCollection",
-        "features": [{
-            "type": "Feature",
-            "properties": { "name": "Null Island" },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [ 121, 31 ]
-          }
-      }]
-    }
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "name": "Null Island"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [121, 31]
+        }
+    }]
+}
--- testSocket.io.js
+++ testSocket.io.js
@@ -2,18 +2,17 @@ var io = require('socket.io-client');
 
 var socket = io.connect('http://133.130.116.215:3000/');
 
-socket.on('complete', function(){
-	console.log('over');
+socket.on('complete', function() {
+    console.log('over');
 })
 
-for (var i = 0; i < 1; i++) {
-	data = {
-		user:"test",
-		PM:Math.random()*3600,
-		TIME:(new Date().getTime()),
-		LOT:121.4339081+Math.random()-0.5,
-		LAT:31.0276681+Math.random()-0.5
-	}
-	socket.emit('senddata', JSON.stringify(data));
+for (var i = 0; i < 100; i++) {
+    data = {
+        user: "test",
+        PM: Math.random() * 3600,
+        TIME: (new Date().getTime()),
+        LOT: 121.4339081 + Math.random() - 0.5,
+        LAT: 31.0276681 + Math.random() - 0.5
+    }
+    socket.emit('senddata', JSON.stringify(data));
 }
-
