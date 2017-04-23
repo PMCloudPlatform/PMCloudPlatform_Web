9442f11e33835ee25cc2eb25a7041e871cb15793
顾尧 <gamesgao@163.com>
2017-04-23 12:04:23 +0800

    将register修改为Vue模式

---
+++ public/javascripts/bind/register.js
@@ -0,0 +1,40 @@
+var registerVm = new Vue({
+    el: '#app',
+    data: {
+        username: "",
+        password: "",
+        repeatPwd: "",
+        warning: ""
+    },
+    methods: {
+        login: function() {
+            location.href = 'login';
+        },
+        identity: function() {
+            if (this.password != this.repeatPwd) {
+                console.log("www");
+                this.warning = "1px solid red";
+            } else {
+                this.warning = "";
+            }
+        },
+        register: function() {
+            if (this.password == this.repeatPwd) {
+                var data = {
+                    "uname": this.username,
+                    "upwd": this.password
+                };
+                console.log(data);
+                this.$http.get('/register/register').then(
+                    function(response) {
+                        console.log(response.data);
+                    },
+                    function(err) {
+                        console.log(err);
+                    }
+                )
+            }
+        }
+    }
+
+})
--- routes/controllers/register.controller.js
+++ routes/controllers/register.controller.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', index);
+router.get('/register', test);
 
 function index(req, res, next) {
-    res.render('index', {
+    res.render('register', {
             title: 'pm2.5 cloud platform',
         }
 
     )
+}
+
+function test(req, res, next) {
+    res.send("456");
 }
 
 module.exports = router;
