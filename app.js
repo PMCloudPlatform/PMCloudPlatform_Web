13b6e7201c254e4539fb26d34835789bf5e56573
顾尧 <gamesgao@163.com>
2017-05-31 20:04:38 +0800

    添加测试代码

--- app.js
+++ app.js
@@ -169,6 +169,13 @@ io.on('connection', function(socket) {
     } else {
         console.log(socket.request.session.username + "Get into the Server");
         socket.emit("hassession");
+        // test
+
+        socket.emit("alarm", {
+            test: "test410"
+        });
+
+        // test
         socket.join(socket.request.session.username);
         socket.on('senddata', function(json) {
             console.log('receive data...');
